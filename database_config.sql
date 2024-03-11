4fa2eca5970027a33c03d8df140eaaa6d1887a0b
Rafał Celiński <dev@rafal.celinski.xyz>
2024-03-11 13:39:00 +0100

    Added users table to database

---
+++ database_config.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    data_created TIMESTAMP DEFAULT current_timestamp
+)
